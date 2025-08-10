ca7bc11e10b0adf6326088ad2876581c4b16fc7f
sajeeb hossain <ashrafulislam070@gmail.com>
2025-08-10 15:51:59 +0600

    now use github JSON url link

--- src/app/services/studio.service.ts
+++ src/app/services/studio.service.ts
@@ -11,8 +11,8 @@ export class StudioService {
 
 
 
-private apiUrl = '/assets/studio-mock-api.json';
-
+// private apiUrl = '/assets/studio-mock-api.json';
+private apiUrl = 'https://gist.githubusercontent.com/rash3dul-islam/88e1565bea2dd1ff9180ff733617a565/raw/684afa147a8e726d7a5e4fdeb390f2d48b35051d/studio-mock-api,json';
 
   constructor(private http: HttpClient) {}
 
