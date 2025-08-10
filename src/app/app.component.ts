4ba8cd1da27dba2416c5458776e3639def2eea84
sajeeb hossain <ashrafulislam070@gmail.com>
2025-08-10 16:04:36 +0600

    header redesign

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,45 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { StudioListComponent } from "./coponent/studio-list/studio-list.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, StudioListComponent, RouterLink]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, StudioListComponent, RouterLink, CommonModule]
 })
 export class AppComponent {
   title = 'bookingApp';
+
+
+
+  userName = 'John Doe';  // example, get from your auth service
+  userInitials = this.getInitials(this.userName);
+
+  isProfileMenuOpen = false;
+
+  toggleProfileMenu() {
+    this.isProfileMenuOpen = !this.isProfileMenuOpen;
+  }
+
+  closeProfileMenu() {
+    // Close menu when focus is lost
+    this.isProfileMenuOpen = false;
+  }
+
+  getInitials(name: string): string {
+    return name
+      .split(' ')
+      .map(n => n[0])
+      .join('')
+      .toUpperCase();
+  }
+
+  logout() {
+    // Implement your logout logic here
+    console.log('Logout clicked');
+  }
+
 }
