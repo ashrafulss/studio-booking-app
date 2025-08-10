0c0e1987abb20af23b397ec4a89cf0c97ef1d8e1
sajeeb hossain <ashrafulislam070@gmail.com>
2025-08-10 12:42:07 +0600

    first commit

---
+++ src/app/studio-list/studio-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-studio-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './studio-list.component.html',
+  styleUrl: './studio-list.component.css'
+})
+export class StudioListComponent {
+
+}
